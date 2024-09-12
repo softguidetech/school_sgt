bd2e16c92d673ef2a31a6d72660e135686c69c1f
softguidetech <96677990+softguidetech@users.noreply.github.com>
2024-09-12 17:44:34 +0300

    Delete school_mngment_sgt directory

--- school_mngment_sgt/controllers/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import assignment_portal
-from . import payslip_portal
-from . import timetable_portal
-from . import exam_timetable_portal
-from . import event_portal
-from . import student_evaluation
